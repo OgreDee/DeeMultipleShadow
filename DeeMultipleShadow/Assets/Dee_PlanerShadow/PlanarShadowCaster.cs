c528929da64e3b78484cdb6bab8b325d2c189d95
Dee <zcd1990917@126.com>
2018-11-05 13:29:13 +0800

    平面阴影

---
+++ DeeMultipleShadow/Assets/Dee_PlanerShadow/PlanarShadowCaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanarShadowCaster : MonoBehaviour {
+
+    [SerializeField]
+    private Transform reciever;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Shader.SetGlobalMatrix("_World2Ground", reciever.worldToLocalMatrix);
+        Shader.SetGlobalMatrix("_Ground2World", reciever.localToWorldMatrix);
+    }
+}
