34b1d67b95ffe6a3d66d8cc5fe9421fd676a8528
Dee <zcd1990917@126.com>
2018-11-08 00:48:35 +0800

    add shadowmap

---
+++ DeeMultipleShadow/Assets/Dee_LightCamera/Dee_LightCamera.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dee_LightCamera : MonoBehaviour {
+    [SerializeField]
+    Light directionalLight;
+    [SerializeField]
+    Shader casterShader;
+    [SerializeField]
+    bool debug;
+    [SerializeField]
+    LayerMask layer;
+
+    Camera lightCamera;
+
+
+
+    void buildLightCamera()
+    {
+        if (directionalLight.type != LightType.Directional)
+            return;
+
+        GameObject cameraObj = new GameObject("_lightCamera_");
+        cameraObj.hideFlags = HideFlags.DontSave;
+        cameraObj.transform.SetPositionAndRotation(directionalLight.transform.position, directionalLight.transform.rotation);
+
+        lightCamera = cameraObj.AddComponent<Camera>();
+        lightCamera.backgroundColor = Color.clear;
+        lightCamera.clearFlags = CameraClearFlags.SolidColor;
+        lightCamera.cullingMask = layer.value;
+        lightCamera.allowHDR = false;
+        lightCamera.allowMSAA = false;
+        lightCamera.orthographic = true;
+
+        lightCamera.farClipPlane = 100;
+
+        lightCamera.enabled = false;
+
+        lightCamera.SetReplacementShader(casterShader, "");
+    }
+
+    private void OnDestroy()
+    {
+        if(lightCamera != null)
+        {
+            Destroy(lightCamera.gameObject);
+        }
+    }
+
+    private void OnPreRender()
+    {
+        if(lightCamera == null)
+        {
+            buildLightCamera();
+            lightCamera.targetTexture = AllocShadowMap();
+        }
+
+        lightCamera.Render();
+
+        Matrix4x4 projectionMatrix = GL.GetGPUProjectionMatrix(lightCamera.projectionMatrix, false);
+        Shader.SetGlobalMatrix("_gWorldToShadow", projectionMatrix * lightCamera.worldToCameraMatrix);
+    }
+
+
+    RenderTexture shadowMapTexture;
+    RenderTexture AllocShadowMap()
+    {
+        if(shadowMapTexture != null)
+        {
+            shadowMapTexture.width = 512;
+            shadowMapTexture.height = 512;
+            shadowMapTexture.depth = 16;
+        }
+        else
+        {
+            shadowMapTexture = new RenderTexture(512, 512, 16);
+        }
+
+        Shader.SetGlobalTexture("_gShadowMapTexture", shadowMapTexture);
+        Shader.SetGlobalFloat("_gShadowStrength", 1);
+
+        return shadowMapTexture;
+    }
+}
